7f56036039cc19560b1549269d887259b063f141
Manish Kc <manishkc2004@gmail.com>
2025-04-15 18:18:44 +0545

    Static

--- Final/Backend/views.py
+++ Final/Backend/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def navbar(request):
+    return render(request, "navbar.html")
